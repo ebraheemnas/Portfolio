81b514b25c40877cb2ead069ee8d1170dc473052
Ebraheem <109612225+ebraheemnas@users.noreply.github.com>
2022-11-26 20:57:17 +0100

    first commit

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,38 @@
+export default{
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+
+    fields: [
+        
+            {
+                name: 'name',
+                title: 'Name', 
+                type: 'string'
+
+        },
+
+            {
+                name: 'name',
+                title: 'Name', 
+                type: 'string'
+
+        },
+
+            {
+                name: 'name',
+                title: 'Name', 
+                type: 'string'
+
+        },
+
+            {
+            name: 'name',
+            title: 'Name', 
+            type: 'string'
+
+        },
+
+    ],
+
+}
