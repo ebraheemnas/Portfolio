9b775d6825f67d0af13b21fd2971956c2ba2ebe1
Ebraheem <109612225+ebraheemnas@users.noreply.github.com>
2022-12-04 23:36:31 +0100

    1

---
+++ frontend_react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+
+const App = () => {
+  return (
+    <h1>App</h1>
+  );
+}
+
+export default App;
