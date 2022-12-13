98fac60f048cd981b2bb325ca0d1902cb94395f0
Ebraheem <109612225+ebraheemnas@users.noreply.github.com>
2022-12-13 01:16:59 +0100

    works sanity

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -4,17 +4,13 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-
 import './Work.scss';
 
-
 const Work = () => {
-
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-
 
   useEffect(() => {
     const query = '*[_type == "works"]';
@@ -25,36 +21,43 @@ const Work = () => {
     });
   }, []);
 
-
-
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
-  }
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
   return (
-  <>
+    <>
+      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-  <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
-
-  <div className="app__work-filter">
-    {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
-      <div
-        key={index}
-        onClick={() => handleWorkFilter(item)}
-        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-      >
-        {item}
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
 
-
-        <motion.div
-              animate={animateCard}
-              transition={{ duration: 0.5, delayChildren: 0.5 }}
-              className="app__work-portfolio"
-            >
-
-{filterWork.map((work, index) => (
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div
               className="app__work-img app__flex"
@@ -66,11 +69,9 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
 
-                
-                <motion.div
+                  <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
@@ -90,26 +91,21 @@ const Work = () => {
                   </motion.div>
                 </a>
               </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
               </div>
-
-              <div className="app__work-content app__flex">
-                    <h4 className="bold-text">{work.title}</h4>
-                    <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
-
-                  <div className="app__work-tag app__flex">
-                    <p className="p-text">{work.tags[0]}</p>
-                  </div>
-              </div>
-
-
-              </div>
-
-              ))}
-        </motion.div>
-        
-
-  </>
-)
-}
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
 
 export default AppWrap(Work, 'work'); 
